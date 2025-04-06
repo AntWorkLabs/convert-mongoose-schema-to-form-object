d1d7aa2df2d3c8e593edfb23bf22f24e42f10df4
tolgaisik <39388550+tolgaisik@users.noreply.github.com>
2025-04-06 04:13:41 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,26 +4,26 @@ import expresss from "express";
 import cors from "cors";
 
 const workSchema = new mongoose.Schema({
-		title: {
-			type: String,
-			required: true,
-		},
-		company: {
-			type: String,
-			required: true,
-		},
-	})
+	title: {
+		type: String,
+		required: true,
+	},
+	company: {
+		type: String,
+		required: true,
+	},
+});
 
 const itemSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-  })
+	name: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+});
 
 /**
  * @type {mongoose.Schema}
@@ -33,6 +33,10 @@ const schema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+  onBoarding: {
+    type: Boolean,
+    default: false,
+  },
 	season: {
 		type: Number,
 		required: true,
@@ -46,36 +50,35 @@ const schema = new mongoose.Schema({
 		ref: "User",
 	},
 	work: {
-    type: workSchema,
-  },
-  items: {
-    type: [itemSchema],
-  },
+		type: workSchema,
+	},
+	items: {
+		type: [itemSchema],
+	},
 });
 
 const UserModel = mongoose.model("User", schema);
 
 const generateFormObjectFromTraversalMongooseSchema = (schema) => {
-    return Object.keys(schema.paths).reduce((acc, key) => {
-			const path = schema.paths[key];
-			if (path.schema) {
-				acc[key] = {
-					instance: path.instance,
-					schema: generateFormObjectFromTraversalMongooseSchema(path.schema),
-				};
-			} else {
-				acc[key] = {
-					instance: path.instance,
-					options: {
-						...path.options,
-						enum: path.enumValues,
-					},
-				};
-			}
-			return acc;
-		}, {});
-
-}
+	return Object.keys(schema.paths).reduce((acc, key) => {
+		const path = schema.paths[key];
+		if (path.schema) {
+			acc[key] = {
+				instance: path.instance,
+				schema: generateFormObjectFromTraversalMongooseSchema(path.schema),
+			};
+		} else {
+			acc[key] = {
+				instance: path.instance,
+				options: {
+					...path.options,
+					enum: path.enumValues,
+				},
+			};
+		}
+		return acc;
+	}, {});
+};
 
 async function main() {
 	const mongo = await MongoMemoryServer.create();
@@ -88,10 +91,31 @@ app.use(expresss.json());
 app.use(cors());
 
 app.listen(3000, () => {
-  main()
+	main();
 });
 
-app.get("/api", async (req, res) => {
-	res.json(getSchemaPaths(UserModel.schema));
+app.get("/api/schema/user", async (req, res) => {
+	res.json(generateFormObjectFromTraversalMongooseSchema(UserModel.schema));
 });
 
+app.post("/api/user", async (req, res) => {
+	const user = new UserModel(req.body);
+	await user.save();
+	res.json(user);
+});
+app.get("/api/user", async (req, res) => {
+	const users = await UserModel.find();
+	res.json(users);
+});
+app.put("/api/user/:id", async (req, res) => {
+  const user = await UserModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(user);
+});
+app.get("/api/user/:id", async (req, res) => {
+	const user = await UserModel.findById(req.params.id);
+	res.json(user);
+});
